a2b6854a80f4d1872b32288c63f0dd92efaac3de
miner233 <110521837+miner233@users.noreply.github.com>
2022-08-04 10:23:52 +0800

    Delete wiki directory

--- wiki/library/代码/Python/2 乘法计算器.py
+++
@@ -1,4 +0,0 @@
-price=5.9
-weight=5
-money=price*weight
-print(money)
--- wiki/library/代码/Python/3 没用的问候程序.py
+++
@@ -1,6 +0,0 @@
-# 定义变量 a 为问候语
-Greetings="你好，"
-# 获取名称，并将值给予 name 变量
-UserName=input("请输入你的名字:")
-# 打印内容
-print(Greetings,UserName)
